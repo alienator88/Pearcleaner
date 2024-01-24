069c2349e83776b36d193be7de3a66fb02bfd348
Alin Lupascu <lupascu.marius@gmail.com>
2024-01-23 17:54:03 -0700

    Create pearcleaner.rb

---
+++ pearcleaner.rb
@@ -0,0 +1,24 @@
+cask "pearcleaner" do
+  #version :latest
+  version "2.3"
+  sha256 :no_check
+
+  # Latest
+  #url "https://github.com/alienator88/Pearcleaner/releases/#{version}/download/Pearcleaner.zip"
+  # Version
+  url "https://github.com/alienator88/Pearcleaner/releases/download/#{version}/Pearcleaner.zip"
+  name "Pearcleaner"
+  desc "Utility to uninstall apps and clean-up leftover files"
+  homepage "https://github.com/alienator88/Pearcleaner"
+
+  depends_on macos: ">= :ventura"
+
+  app "Pearcleaner.app"
+
+  uninstall quit:      "com.alienator88.Pearcleaner",
+            launchctl: "com.alienator88.PearcleanerSentinel"
+
+  zap trash: [
+    "~/Library/Application Support/Pearcleaner",
+  ]
+end
